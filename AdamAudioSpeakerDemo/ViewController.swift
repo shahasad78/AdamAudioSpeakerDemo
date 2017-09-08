d33c850f708a00ac97b7c51658337a36af0e41a6
Shah Martinez <shahasad78@gmail.com>
2017-09-07 23:41:28 -0700

    Initial Commit

---
+++ AdamAudioSpeakerDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AdamAudioSpeakerDemo
+//
+//  Created by Richard Martinez on 9/7/17.
+//  Copyright © 2017 Phantom Universal Media Productions. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
